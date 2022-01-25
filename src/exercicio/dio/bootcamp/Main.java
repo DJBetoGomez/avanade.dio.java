511a3c0ac52525d2a002b69e5f803b48146f1a95
robertoferreira <rgdferreira@hotmail.com>
2022-01-25 17:32:00 -0300

    new lines

--- src/exercicio/dio/bootcamp/Main.java
+++ src/exercicio/dio/bootcamp/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         int i1 = 10;
-        int i2 = 20;
-        float f1 =4.5f;
-        float f2 = 3.5f;
+        int i2 = 5;
+        float f1 =20f;
+        float f2 = 50f;
         double d1 = 59.6d;
         char c1 = 'x';
         char c2 = 'y';
@@ -47,12 +47,38 @@ public class Main {
         //System.out.println(b1 > b2);
         //System.out.println(b1 <= i1);
 
+        boolean b3 = true;
+        boolean b4 = false;
 
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
 
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
 
+        System.out.println("b1 ˆ b3 " + (b1 ^ b3));
+        System.out.println("b4 ˆ b1 " + (b4 ^ b1));
 
+        System.out.println(!b1);
+        System.out.println(!b2);
 
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));
+        System.out.println("((i1 > i2) || (f2 < f1) " + ((i1 > i2) || (f2 < f1)));
 
+        double salarioMensal = 11893.52d;
+        double mediaSalario = 10500d;
 
+        int quantidadeDependentes =4;
+        int mediaDependentes =2;
+
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilio = ((salarioBaixo) && (muitosDependentes));
+        System.out.println("recebeAuxilio " + recebeAuxilio);
     }
 }
